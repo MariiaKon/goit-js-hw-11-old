deec369c498c4d3472ecca6a02e1bd0b5cdca089
Mariia Konovalchuk <71904680+MariiaKon@users.noreply.github.com>
2021-08-01 19:28:03 +0300

    Add color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,16 +1,27 @@
-const startBtn = document.querySelector('[data-start]')
-const stopBtn = document.querySelector('[data-stop]')
+; const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.querySelector('.colorSwitcherBody');
 
-// startBtn.addEventListener('click',)
-// stopBtn.addEventListener('click',)
-
-
-
-
-
+startBtn.addEventListener('click', startRandom);
+stopBtn.addEventListener('click', stopRandom);
 
 
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
 
+function startRandom() {
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+
+    interval = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+};
+
+function stopRandom () {
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+
+    clearInterval(interval)
+};
