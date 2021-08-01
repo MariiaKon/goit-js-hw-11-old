72600c1508cab34430cce5eb2f02eefae83bbf75
Mariia Konovalchuk <71904680+MariiaKon@users.noreply.github.com>
2021-08-01 18:13:54 +0300

    Add start files

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,16 @@
+const startBtn = document.querySelector('[data-start]')
+const stopBtn = document.querySelector('[data-stop]')
+
+// startBtn.addEventListener('click',)
+// stopBtn.addEventListener('click',)
+
+
+
+
+
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
